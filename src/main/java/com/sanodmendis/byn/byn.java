86d2d1e37ae6b409ab77c723310b8e0a648f26e7
sanodmendis <sanodmendis@gmail.com>
2025-11-24 01:45:28 +0530

    init

---
+++ src/main/java/com/sanodmendis/byn/byn.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sanodmendis.byn;
+
+/**
+ *
+ * @author Sanod D. Mendis
+ */
+public class byn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
